2f40370704862847d16c5bb956404063eccbef62
Sezgin Mustafa <sezgin_mustafa@outlook.com>
2023-03-31 16:24:27 +0300

    Added IsHiddenSingleInRow

---
+++ SudokuSolver.Test.Uni/Strategies/HiddenSingleStrategyTest.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SudokuSolver.Test.Unit.Strategies;
+using SudokuSolver.Strategies;
+using SudokuSolver.Workers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver.Test.Unit.Strategies.Tests
+{
+    [TestClass]
+    public class HiddenSingleStrategyTest
+    {
+        private readonly HiddenSingleStrategy _hiddenSingleStrategy = new(new SudokuMapper());
+        private readonly SudokuBoardStateManager _sudokuBoardStateManager = new SudokuBoardStateManager();
+
+        internal int[,] sudokuBoard = 
+            {
+                { 123, 2345, 34, 23, 344, 62, 72, 348, 96 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 2, 2345, 345, 23, 344, 62, 2, 34, 6 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            };
+
+        [TestMethod]
+        public void CleanHiddenSingleInRow_HiddenSinglesExist_SolvesFirstHiddenSingle()
+        {
+            var currentState = _sudokuBoardStateManager.GenerateState(sudokuBoard);
+            _hiddenSingleStrategy.CleanHiddenSingleInRow(sudokuBoard, 0);
+            var nextState = _sudokuBoardStateManager.GenerateState(sudokuBoard);
+            Assert.AreNotEqual(nextState, currentState);
+        }
+
+        [TestMethod]
+        public void CleanHiddenSingleInRow_NoHiddenSingles_DoesntChangeBoardState()
+        {
+            var currentState = _sudokuBoardStateManager.GenerateState(sudokuBoard);
+            _hiddenSingleStrategy.CleanHiddenSingleInRow(sudokuBoard, 5);
+            var nextState = _sudokuBoardStateManager.GenerateState(sudokuBoard);
+            Assert.AreEqual(nextState, currentState);
+        }
+
+        [TestMethod()]
+        public void HasHiddenSingleInRow_HiddenSingleExists_ReturnsFirstHiddenSingle()
+        {
+            var hiddenSingle = _hiddenSingleStrategy.HasHiddenSingleInRow(sudokuBoard, 0);
+            Assert.IsTrue(hiddenSingle.Single != -1);
+            Assert.IsTrue(hiddenSingle.Single == 1);
+        }
+
+
+        // NonHiddenSingle: (-1, -1, -1)
+        [TestMethod()]
+        public void HasHiddenSingleInRow_HiddenSingleExists_ReturnsNonHiddenSingle()
+        {
+            var hiddenSingle = _hiddenSingleStrategy.HasHiddenSingleInRow(sudokuBoard, 5);
+            Assert.IsTrue(hiddenSingle.Single == -1);
+        }
+        [TestMethod()]
+        public void HasHiddenSingleInRow_EmptyRow_ReturnsNonHiddenSingle()
+        {
+            var hiddenSingle = _hiddenSingleStrategy.HasHiddenSingleInRow(sudokuBoard, 1);
+            Assert.IsTrue(hiddenSingle.Single == -1);
+        }
+
+        [TestMethod]
+        public void IsHiddenSingle_HiddenSingle_ReturnsTrue()
+        {
+            var result = _hiddenSingleStrategy.IsHiddenSingleInRow(sudokuBoard, 0, 0, '1');
+            Assert.IsTrue(result);
+
+            result = _hiddenSingleStrategy.IsHiddenSingleInRow(sudokuBoard, 0, 1, '5');
+            Assert.IsTrue(result);
+
+        }
+        [TestMethod]
+        public void IsHiddenSingle_NonHiddenSingle_ReturnsFalse()
+        {
+            var result = _hiddenSingleStrategy.IsHiddenSingleInRow(sudokuBoard, 0, 1, '3');
+            Assert.IsFalse(result);
+
+            result = _hiddenSingleStrategy.IsHiddenSingleInRow(sudokuBoard, 0, 8, '6');
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ SudokuSolver/Strategies/HiddenSingleStrategy.cs
@@ -0,0 +1,100 @@
+﻿using SudokuSolver.Strategies;
+using SudokuSolver.Workers;
+
+namespace SudokuSolver.Test.Unit.Strategies
+{
+    internal class HiddenSingleStrategy : ISudokuStrategy
+    {
+        private readonly SudokuMapper _sudokuMapper;
+        public HiddenSingleStrategy(SudokuMapper sudokuMapper)
+        {
+            _sudokuMapper = sudokuMapper;
+        }
+
+        public int[,] Solve(int[,] sudokuBoard)
+        {
+            for (int row = 0; row < sudokuBoard.GetLength(0); row++)
+            {
+                CleanHiddenSingleInRow(sudokuBoard, row);
+            }
+
+            for (int col = 0; col < sudokuBoard.GetLength(0); col++)
+            {
+                //CleanHiddenSingleInCol(sudokuBoard, col);
+            }
+
+            for (int row = 0; row < sudokuBoard.GetLength(0); row++)
+            {
+                for (int col = 0; col < sudokuBoard.GetLength(1); col++)
+                {  
+
+                }
+            }
+            return sudokuBoard;
+        }
+
+        /*
+         * 
+         * Traverse the whole row
+         *      For each note digit check all cells in the row
+         *      If no other cell has that digit 
+         *          Remove all other digits in the cell except the hidden single
+         *      If no hidden single found return -1
+         * 
+         * */
+
+        internal void CleanHiddenSingleInRow(int[,] sudokuBoard, int givenRow)
+        {
+            var hiddenSingle = HasHiddenSingleInRow(sudokuBoard, givenRow);
+
+            if (hiddenSingle.Single != -1) 
+                sudokuBoard[givenRow, hiddenSingle.Col] = hiddenSingle.Single;
+            
+            
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sudokuBoard">The represantation of the sudoku board.</param>
+        /// <param name="givenRow"></param>
+        /// <param name="givenCol"></param>
+        /// <returns>If no hidden single found in the row returns (-1, -1, -1), otherwise returns hidden single and its row and column.</returns>
+        public (int Single, int Row, int Col) HasHiddenSingleInRow(int[,] sudokuBoard, int givenRow)
+        {
+            for (int col = 0; col < sudokuBoard.GetLength(1); col++)
+            {
+                var cell = sudokuBoard[givenRow, col];
+                var possibilities = cell.ToString().ToCharArray();
+                foreach (var possibility in possibilities)
+                {
+                    if (IsHiddenSingleInRow(sudokuBoard, givenRow, col, possibility))
+                    {
+                        return (Single: Convert.ToInt16(possibility.ToString()),  Row: givenRow, Col: col);
+                    }
+                }
+            }
+
+            return (-1, -1, -1);
+        }
+        /// <summary>
+        /// Checks it
+        /// </summary>
+        /// <param name="possibility"></param>
+        /// <returns></returns>
+        internal bool IsHiddenSingleInRow(int[,] sudokuBoard, int givenRow, int givenCol, char possibility)
+        {
+
+            for (int col = 0; col < sudokuBoard.GetLength(1); col++)
+            {
+                if (col == givenCol) continue;
+                var cell = sudokuBoard[givenRow, col];
+                var possibilities = cell.ToString().ToCharArray();
+                if (possibilities.Any(p => p == possibility) || possibilities.Length < 2) return false;
+
+            }
+
+            return true;
+        }
+    }
+}
