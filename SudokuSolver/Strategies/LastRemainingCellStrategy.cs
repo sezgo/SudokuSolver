eef99c951aca208f056386af968183232c7c8472
Sezgin Mustafa <sezgin_mustafa@outlook.com>
2023-04-10 21:20:54 +0300

    Seperated last remaining cell strategy into its own from from simple mark up strategies

---
+++ SudokuSolver/Strategies/LastRemainingCellStrategy.cs
@@ -0,0 +1,23 @@
+﻿using SudokuSolver.Test.Unit.Strategies;
+using SudokuSolver.Workers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver.Strategies
+{
+    internal class LastRemainingCellStrategy : ISudokuStrategy
+    {
+        public int[,] Solve(int[,] sudokuBoard)
+        {
+            new PossibilityCalculator(new SudokuMapper()).Calculate(sudokuBoard);
+            new HiddenSinglesStrategy(new SudokuMapper()).Solve(sudokuBoard);
+
+            return sudokuBoard;
+        }
+        
+
+    }
+}
