219fb05949e7fc322f7252bf7c44671039b9421d
Sezgin Mustafa <sezgin_mustafa@outlook.com>
2023-04-10 21:21:54 +0300

    Seperated last possible number strategy into its own from from simple mark up strategies

---
+++ SudokuSolver/Strategies/LastPossibleNumberStrategy.cs
@@ -0,0 +1,19 @@
+﻿using SudokuSolver.Workers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver.Strategies
+{
+    internal class LastPossibleNumberStrategy : ISudokuStrategy
+    {
+        public int[,] Solve(int[,] sudokuBoard)
+        {
+            new PossibilityCalculator(new SudokuMapper()).Calculate(sudokuBoard);
+
+            return sudokuBoard;
+        }
+    }
+}
