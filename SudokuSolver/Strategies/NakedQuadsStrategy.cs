76de3aa19535bc952d74a65b682ac0b08631f01c
Sezgin Mustafa <sezgin_mustafa@outlook.com>
2023-04-15 19:19:58 +0300

    Naked quad strategy now solves naked quads on column and on block
    Implemented:
    - SolveNakedQuadForInBlock
    - HasNakedQuadInBlock
    - SolveNakedQuadForInCol
    - HasNakedQuadInCol
    - AreSameCells
    
    AreSameCells check used in row, column and block methods and checks if
    the given indexes are the same using a hash set.
    index : row, col or cellIndex(for blocks).

--- SudokuSolver/Strategies/NakedQuadsStrategy.cs
+++ SudokuSolver/Strategies/NakedQuadsStrategy.cs
@@ -1,4 +1,5 @@
-﻿using SudokuSolver.Workers;
+﻿using SudokuSolver.Data;
+using SudokuSolver.Workers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,14 +17,123 @@ namespace SudokuSolver.Strategies
         }
         public int[,] Solve(int[,] sudokuBoard)
         {
-            for (int col = 0; col < sudokuBoard.GetLength(0); col++)
+            for (int row = 0; row < sudokuBoard.GetLength(0); row++)
             {
-                for (int row = 0; row < sudokuBoard.GetLength(1); row++)
+                for (int col = 0; col < sudokuBoard.GetLength(1); col++)
                 {
                     SolveNakedQuadForInRow(sudokuBoard, row, col);
+                    SolveNakedQuadForInCol(sudokuBoard, row, col);
+                    SolveNakedQuadForInBlock(sudokuBoard, row, col);
                 }
             }
             return sudokuBoard;
+        }
+
+        private void SolveNakedQuadForInBlock(int[,] sudokuBoard, int givenRow, int givenCol)
+        {
+            var nakedQuad = HasNakedQuadInBlock(sudokuBoard, givenRow, givenCol);
+            if (!nakedQuad.IsNakedQuad) return;
+
+            SudokuMap blockMap = _sudokuMapper.Find(givenRow, givenCol);
+
+            var givenCellIndex = _sudokuMapper.GetCellIndex(givenRow, givenCol);
+
+            for (int cellIndex = 0; cellIndex < sudokuBoard.GetLength(0); cellIndex++)
+            {
+                var cellRow = _sudokuMapper.GetCellRow(cellIndex, blockMap);
+                var cellCol = _sudokuMapper.GetCellCol(cellIndex, blockMap);
+                var cell = sudokuBoard[cellRow, cellCol];
+
+                if (nakedQuad.First != cell && nakedQuad.Second != cell && nakedQuad.Third != cell && nakedQuad.Fourth != cell)
+                {
+                    var strValuesToEliminate = nakedQuad.First.ToString() + nakedQuad.Second.ToString() + nakedQuad.Third.ToString() + nakedQuad.Fourth.ToString();
+                    EliminateNakedQuad(sudokuBoard, strValuesToEliminate, cellRow, cellCol);
+                }
+            }
+        }
+
+        private (int First, int Second, int Third, int Fourth, bool IsNakedQuad) HasNakedQuadInBlock(int[,] sudokuBoard, int givenRow, int givenCol)
+        {
+            SudokuMap blockMap = _sudokuMapper.Find(givenRow, givenCol);
+
+            var givenCellIndex = _sudokuMapper.GetCellIndex(givenRow, givenCol);
+
+            for (int secondCellIndex = 0; secondCellIndex < sudokuBoard.GetLength(0); secondCellIndex++)
+            {
+                for (int thirdCellIndex = 0; thirdCellIndex < sudokuBoard.GetLength(0); thirdCellIndex++)
+                {
+                    for (int fourthCellIndex = 0; fourthCellIndex < sudokuBoard.GetLength(0); fourthCellIndex++)
+                    {
+                        if (!AreSameCells(givenCellIndex, secondCellIndex, thirdCellIndex, fourthCellIndex))
+                        {
+                            var secondRow = _sudokuMapper.GetCellRow(secondCellIndex, blockMap);
+                            var secondCol = _sudokuMapper.GetCellCol(secondCellIndex, blockMap);
+
+                            var thirdRow = _sudokuMapper.GetCellRow(thirdCellIndex, blockMap);
+                            var thirdCol = _sudokuMapper.GetCellCol(thirdCellIndex, blockMap);
+
+                            var fourthRow = _sudokuMapper.GetCellRow(fourthCellIndex, blockMap);
+                            var fourthCol = _sudokuMapper.GetCellCol(fourthCellIndex, blockMap);
+
+                            if (IsNakedQuad(sudokuBoard[givenRow, givenCol],
+                                sudokuBoard[secondRow, secondCol],
+                                sudokuBoard[thirdRow, thirdCol],
+                                sudokuBoard[fourthRow, fourthCol]))
+                            {
+                                return (
+                                    First: sudokuBoard[givenRow, givenCol],
+                                    Second: sudokuBoard[secondRow, secondCol],
+                                    Third: sudokuBoard[thirdRow, thirdCol],
+                                    Fourth: sudokuBoard[fourthRow, fourthCol],
+                                    IsNakedQuad: true
+                                    );
+                            }
+                        }
+                    }
+                }
+            }
+
+            return (-1, -1, -1, -1, false);
+        }
+
+        private void SolveNakedQuadForInCol(int[,] sudokuBoard, int givenRow, int givenCol)
+        {
+            var nakedQuad = HasNakedQuadInCol(sudokuBoard, givenRow, givenCol);
+            if (!nakedQuad.IsNakedQuad) return;
+            for (int row = 0; row < sudokuBoard.GetLength(1); row++)
+            {
+                var cell = sudokuBoard[row, givenCol];
+                if (nakedQuad.First != cell && nakedQuad.Second != cell && nakedQuad.Third != cell && nakedQuad.Fourth != cell)
+                {
+                    var strValuesToEliminate = nakedQuad.First.ToString() + nakedQuad.Second.ToString() + nakedQuad.Third.ToString() + nakedQuad.Fourth.ToString();
+                    EliminateNakedQuad(sudokuBoard, strValuesToEliminate, row, givenCol);
+                }
+            }
+        }
+
+        private (int First, int Second, int Third, int Fourth, bool IsNakedQuad) HasNakedQuadInCol(int[,] sudokuBoard, int givenRow, int givenCol)
+        {
+            for (int row2 = 0; row2 < sudokuBoard.GetLength(0); row2++)
+            {
+                for (int row3 = 0; row3 < sudokuBoard.GetLength(0); row3++)
+                {
+                    for (int row4 = 0; row4 < sudokuBoard.GetLength(0); row4++)
+                    {
+                        if (!AreSameCells(givenRow, row2, row3, row4) &&
+                            IsNakedQuad(sudokuBoard[givenRow, givenCol], sudokuBoard[row2, givenCol], sudokuBoard[row3, givenCol], sudokuBoard[row4, givenCol]))
+                        {
+                            return (
+                                First: sudokuBoard[givenRow, givenCol],
+                                Second: sudokuBoard[row2, givenCol],
+                                Third: sudokuBoard[row3, givenCol],
+                                Fourth: sudokuBoard[row4, givenCol],
+                                IsNakedQuad: true
+                            );
+                        }
+                    }
+                }
+            }
+            return (-1, -1, -1, -1, false);
         }
 
         private void SolveNakedQuadForInRow(int[,] sudokuBoard, int givenRow, int givenCol)
@@ -49,7 +159,7 @@ namespace SudokuSolver.Strategies
                 {
                     for (int col4 = 0; col4 < sudokuBoard.GetLength(1); col4++)
                     {
-                        if (new HashSet<int>(new int[] {givenCol, col2, col3, col4}).Count == 4 &&
+                        if (!AreSameCells(givenCol, col2, col3, col4) &&
                             IsNakedQuad(sudokuBoard[givenRow, givenCol], sudokuBoard[givenRow, col2], sudokuBoard[givenRow, col3], sudokuBoard[givenRow, col4]))
                         {
                             return (
@@ -98,6 +208,11 @@ namespace SudokuSolver.Strategies
             set.UnionWith(strFourthNaked);
 
             return set.Count == 4;
+        }
+
+        public bool AreSameCells(int index1, int index2, int index3, int index4)
+        {
+            return new HashSet<int> { index1, index2, index3, index4}.Count != 4;
         }
     }
 }
